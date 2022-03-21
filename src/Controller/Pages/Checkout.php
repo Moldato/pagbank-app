ff8bc61615e32f65615ce613160e909dd7f1349c
Alyson Souza <alysonforever@gmail.com>
2022-03-21 11:44:24 -0300

    add pagbank-sdk-php

--- src/Controller/Pages/Checkout.php
+++ src/Controller/Pages/Checkout.php
@@ -3,10 +3,19 @@ namespace App\Controller\Pages;
 
 use App\Utils\View;
 
+use Moldato\PagBankSDK\Credentials;
+use Moldato\PagBankSDK\Session;
+use Moldato\PagBankSDK\Prepare;
+
 class Checkout extends Page {
 
   private static function loadJavaScript(): array {
-    $scripts = [];
+    $credential = new Credentials;
+    $credential->email = 'alysonforever@gmail.com';
+    $credential->token = '953D361099A44089B60B355A9084D72F';
+    $session = new Session($credential);
+    $prepare = new Prepare($session->create());
+    $scripts = $prepare->initPagBankLib();
     return $scripts;
   }
 
@@ -18,3 +27,7 @@ class Checkout extends Page {
     return self::getPage('Checkout', $content, self::loadJavaScript() );
   }
 }
+
+
+echo '<pre>';
+echo '</pre>';
